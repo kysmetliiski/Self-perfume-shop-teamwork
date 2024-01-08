b625a9c558103c7e482ca31d7b1646069f5053f8
kysmetliiski <19507@uktc-bg.com>
2024-01-08 18:24:07 +0200

    female perfumes added

---
+++ controller/mproductf.php
@@ -0,0 +1,19 @@
+<?php 
+
+class mproductf extends Controller
+{
+    function __construct()
+    {
+        
+        parent::__construct();
+    }
+    public function index()
+    {
+        
+
+        $this->view->render("multiple-product-female.html");
+    }
+}
+
+
+?> 
